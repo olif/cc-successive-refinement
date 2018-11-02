c8e14a2191a07589dca9f5903762e747f93efd1e
Niklas Olofsson <niklas.olofsson@olif.nu>
2018-11-02 14:55:49 +0100

    Add tests

--- args.go
+++ args.go
@@ -1,7 +1,5 @@
 package main
 
-// Create a list for all errors encountered
-
 import (
 	"bytes"
 	"fmt"
@@ -10,61 +8,13 @@ import (
 	"unicode"
 )
 
-const (
-	// ErrorCodeOk - ok code
-	ErrorCodeOk = 0
-	// ErrorCodeUnexpectedArgument -
-	ErrorCodeUnexpectedArgument = 1
-	// ErrorCodeMissingString  - missing string code
-	ErrorCodeMissingString = 2
-	// ErrorCodeMissingInteger  - missing integer code
-	ErrorCodeMissingInteger = 3
-	// ErrorCodeInvalidInteger  - invalid integer code
-	ErrorCodeInvalidInteger = 4
-	// ErrorCodeMissingFloat  - missing float code
-	ErrorCodeMissingFloat = 5
-	// ErrorCodeInvalidFloat  - invalid float code
-	ErrorCodeInvalidFloat = 6
-	// ErrorCodeInvalidArgumentName  - invalid name code
-	ErrorCodeInvalidArgumentName = 7
-)
-
-type argsErrorCode int
-
-type argsError struct {
-	errorArgumentID rune
-	errorParameter  string
-	errorCode       argsErrorCode
-}
-
-func (e *argsError) Error() string {
-	switch e.errorCode {
-	case ErrorCodeOk:
-		return fmt.Sprintf("TILT: Should not get here")
-	case ErrorCodeUnexpectedArgument:
-		return fmt.Sprintf("Argument -%s unexpected", string(e.errorArgumentID))
-	case ErrorCodeMissingString:
-		return fmt.Sprintf("Could not find string parameter for -%s", string(e.errorArgumentID))
-	case ErrorCodeInvalidInteger:
-		return fmt.Sprintf("Argument -%s expects an integer but was '%s'", string(e.errorArgumentID), e.errorParameter)
-	case ErrorCodeInvalidFloat:
-		return fmt.Sprintf("Argument -%s expects a float but was '%s'", string(e.errorArgumentID), e.errorParameter)
-	case ErrorCodeMissingFloat:
-		return fmt.Sprintf("Could not find double parameter for -%s", string(e.errorArgumentID))
-	case ErrorCodeInvalidArgumentName:
-		return fmt.Sprintf("'%s' is not a valid argument name", string(e.errorArgumentID))
-	}
-
-	return ""
-}
-
-// Args -
+// Args - An arg parser supporting bool, int, string and float values
 type Args struct {
 	schema              string
 	args                []string
 	unexpectedArguments []rune
 	argsFound           []rune
-	marhalers           map[rune]ArgumentMarshaler
+	marhalers           map[rune]argumentMarshaler
 	currentArgument     *iterator
 }
 
@@ -75,138 +25,11 @@ func NewArgs(schema string, args []string) (*Args, error) {
 		args:                args,
 		unexpectedArguments: make([]rune, 0),
 		argsFound:           make([]rune, 0),
-		marhalers:           make(map[rune]ArgumentMarshaler),
+		marhalers:           map[rune]argumentMarshaler{},
 		currentArgument:     nil,
 	}
 
 	return &a, a.parse()
-}
-
-func (a *Args) parse() error {
-	if len(a.schema) == 0 && len(a.args) == 0 {
-		return nil
-	}
-	if err := a.parseSchema(); err != nil {
-		return err
-	}
-	if err := a.parseArguments(); err != nil {
-		return err
-	}
-	//return len(a.unexpectedArguments) == 0, nil
-	return nil
-}
-
-func (a *Args) parseSchema() error {
-	for _, element := range strings.Split(a.schema, ",") {
-		if len(element) > 0 {
-			trimmedElement := strings.TrimSpace(element)
-			if err := a.parseSchemaElement(trimmedElement); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-func (a *Args) parseSchemaElement(element string) error {
-	elementID := rune(element[0])
-	elementTail := element[1:]
-	if err := a.validateSchemaElement(elementID); err != nil {
-		return err
-	}
-
-	if len(elementTail) == 0 {
-		a.marhalers[elementID] = &booleanArgumentMarshaler{}
-	} else if elementTail == "*" {
-		a.marhalers[elementID] = &stringArgumentMarshaler{}
-	} else if elementTail == "#" {
-		a.marhalers[elementID] = &integerArgumentMarshaler{}
-	} else if elementTail == "##" {
-		a.marhalers[elementID] = &floatArgumentMarshaler{}
-	} else {
-		return fmt.Errorf("%s has invalid format: %s", string(elementID), elementTail)
-	}
-
-	return nil
-}
-
-func (a *Args) validateSchemaElement(elementID rune) error {
-	if !unicode.IsLetter(elementID) {
-		return fmt.Errorf("Bad characted %s in Args format: %s", string(elementID), a.schema)
-	}
-	return nil
-}
-
-func (a *Args) parseArguments() error {
-	for a.currentArgument = newIterator(a.args); a.currentArgument.hasNext(); {
-		arg := a.currentArgument.next()
-		if err := a.parseArgument(arg); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func (a *Args) parseArgument(arg string) error {
-	if string(arg[0]) == "-" {
-		return a.parseElements(arg)
-	}
-	return nil
-}
-
-func (a *Args) parseElements(arg string) error {
-	for i := 1; i < len(arg); i++ {
-		if err := a.parseElement(rune(arg[i])); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func (a *Args) parseElement(argChar rune) error {
-	if err := a.setArgument(argChar); err == nil {
-		a.argsFound = append(a.argsFound, argChar)
-	} else {
-		a.unexpectedArguments = append(a.unexpectedArguments, argChar)
-		argsErr := err.(*argsError)
-		argsErr.errorArgumentID = argChar
-		return argsErr
-	}
-
-	return nil
-}
-
-func (a *Args) setArgument(argChar rune) error {
-	m, ok := a.marhalers[argChar]
-	if !ok {
-		return fmt.Errorf("Not a valid arg type")
-	}
-	return m.set(a.currentArgument)
-}
-
-// Cardinality - Returns the number of arguments
-func (a *Args) Cardinality() int {
-	return len(a.argsFound)
-}
-
-// Usage - Returns a string describing the usage
-func (a *Args) Usage() string {
-	if len(a.schema) > 0 {
-		return fmt.Sprintf("[%s]", a.schema)
-	}
-	return ""
-}
-
-func (a *Args) unexpectedArgumentMessage() string {
-	var buffer bytes.Buffer
-	buffer.WriteString("Argument(s) -")
-	for _, v := range a.unexpectedArguments {
-		buffer.WriteRune(v)
-	}
-	buffer.WriteString(" unexpected.")
-
-	return buffer.String()
 }
 
 // GetBoolean - Returns the value of the boolean arg
@@ -253,56 +76,249 @@ func (a *Args) Has(arg rune) bool {
 	return false
 }
 
-// ArgumentMarshaler - Parses cmd line arguments and makes them retrievable by key
-type ArgumentMarshaler interface {
+// Cardinality - Returns the number of arguments
+func (a *Args) Cardinality() int {
+	return len(a.argsFound)
+}
+
+// Usage - Returns a string describing the usage
+func (a *Args) Usage() string {
+	if len(a.schema) > 0 {
+		return fmt.Sprintf("[%s]", a.schema)
+	}
+	return ""
+}
+
+func (a *Args) parse() error {
+	if len(a.schema) == 0 && len(a.args) == 0 {
+		return nil
+	}
+	if err := a.parseSchema(); err != nil {
+		return err
+	}
+	if err := a.parseArguments(); err != nil {
+		return err
+	}
+	if len(a.unexpectedArguments) != 0 {
+		return &ArgsError{errorCode: ErrUnexpectedArgument}
+	}
+	//return len(a.unexpectedArguments) == 0, nil
+	return nil
+}
+
+func (a *Args) parseSchema() error {
+	for _, element := range strings.Split(a.schema, ",") {
+		if len(element) > 0 {
+			trimmedElement := strings.TrimSpace(element)
+			if err := a.parseSchemaElement(trimmedElement); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (a *Args) parseSchemaElement(element string) error {
+	elementID := rune(element[0])
+	elementTail := element[1:]
+	if err := a.validateSchemaElement(elementID); err != nil {
+		return err
+	}
+
+	if len(elementTail) == 0 {
+		a.marhalers[elementID] = &booleanargumentMarshaler{}
+	} else if elementTail == "*" {
+		a.marhalers[elementID] = &stringargumentMarshaler{}
+	} else if elementTail == "#" {
+		a.marhalers[elementID] = &integerargumentMarshaler{}
+	} else if elementTail == "##" {
+		a.marhalers[elementID] = &floatargumentMarshaler{}
+	} else {
+		return &ArgsError{
+			errorCode:       ErrInvalidFormat,
+			errorArgumentID: elementID,
+			errorParameter:  elementTail,
+		}
+	}
+
+	return nil
+}
+
+func (a *Args) validateSchemaElement(elementID rune) error {
+	if !unicode.IsLetter(elementID) {
+		return &ArgsError{
+			errorCode:       ErrInvalidArgumentName,
+			errorArgumentID: elementID,
+		}
+	}
+	return nil
+}
+
+func (a *Args) parseArguments() error {
+	for a.currentArgument = newIterator(a.args); a.currentArgument.hasNext(); {
+		arg := a.currentArgument.next()
+		if err := a.parseArgument(arg); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (a *Args) parseArgument(arg string) error {
+	if string(arg[0]) == "-" {
+		return a.parseElements(arg)
+	}
+	return nil
+}
+
+func (a *Args) parseElements(arg string) error {
+	for i := 1; i < len(arg); i++ {
+		if err := a.parseElement(rune(arg[i])); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (a *Args) parseElement(argChar rune) error {
+	if err := a.setArgument(argChar); err == nil {
+		a.argsFound = append(a.argsFound, argChar)
+	} else {
+		a.unexpectedArguments = append(a.unexpectedArguments, argChar)
+		argsErr := err.(*ArgsError)
+		argsErr.errorArgumentID = argChar
+		return argsErr
+	}
+	return nil
+}
+
+func (a *Args) setArgument(argChar rune) error {
+	m, ok := a.marhalers[argChar]
+	if !ok {
+		return &ArgsError{
+			errorCode:       ErrUnexpectedArgument,
+			errorArgumentID: argChar,
+		}
+	}
+	return m.set(a.currentArgument)
+}
+
+func (a *Args) unexpectedArgumentMessage() string {
+	var buffer bytes.Buffer
+	buffer.WriteString("Argument(s) -")
+	for _, v := range a.unexpectedArguments {
+		buffer.WriteRune(v)
+	}
+	buffer.WriteString(" unexpected.")
+
+	return buffer.String()
+}
+
+const (
+	ErrOk = iota
+	ErrUnexpectedArgument
+	ErrMissingString
+	ErrMissingInteger
+	ErrInvalidInteger
+	ErrMissingFloat
+	ErrInvalidFloat
+	ErrInvalidArgumentName
+	ErrInvalidFormat
+)
+
+// ArgsError - Args error type
+type ArgsError struct {
+	errorArgumentID rune
+	errorParameter  string
+	errorCode       int
+}
+
+func (e *ArgsError) Error() string {
+	switch e.errorCode {
+	case ErrOk:
+		return fmt.Sprintf("TILT: Should not get here")
+	case ErrUnexpectedArgument:
+		return fmt.Sprintf("Argument -%s unexpected", string(e.errorArgumentID))
+	case ErrMissingString:
+		return fmt.Sprintf("Could not find string parameter for -%s", string(e.errorArgumentID))
+	case ErrInvalidInteger:
+		return fmt.Sprintf("Argument -%s expects an integer but was '%s'", string(e.errorArgumentID), e.errorParameter)
+	case ErrInvalidFloat:
+		return fmt.Sprintf("Argument -%s expects a float but was '%s'", string(e.errorArgumentID), e.errorParameter)
+	case ErrMissingFloat:
+		return fmt.Sprintf("Could not find double parameter for -%s", string(e.errorArgumentID))
+	case ErrInvalidArgumentName:
+		return fmt.Sprintf("'%s' is not a valid argument name", string(e.errorArgumentID))
+	}
+
+	return ""
+}
+
+// Code - The error code
+func (e *ArgsError) Code() int {
+	return e.errorCode
+}
+
+// ArgumentID - The argument that is invalid
+func (e *ArgsError) ArgumentID() rune {
+	return e.errorArgumentID
+}
+
+// Parameter - The invalid parameter
+func (e *ArgsError) Parameter() string {
+	return e.errorParameter
+}
+
+// argumentMarshaler - Parses cmd line arguments and makes them retrievable by key
+type argumentMarshaler interface {
 	get() interface{}
 	set(i *iterator) error
 }
 
-type booleanArgumentMarshaler struct {
+type booleanargumentMarshaler struct {
 	boolVal bool
 }
 
-func (b *booleanArgumentMarshaler) set(i *iterator) error {
+func (b *booleanargumentMarshaler) set(i *iterator) error {
 	b.boolVal = true
 	return nil
 }
 
-func (b *booleanArgumentMarshaler) get() interface{} {
+func (b *booleanargumentMarshaler) get() interface{} {
 	return b.boolVal
 }
 
-type stringArgumentMarshaler struct {
+type stringargumentMarshaler struct {
 	stringVal string
 }
 
-func (s *stringArgumentMarshaler) set(i *iterator) error {
+func (s *stringargumentMarshaler) set(i *iterator) error {
 	if !i.hasNext() {
-		return &argsError{errorCode: ErrorCodeMissingString}
+		return &ArgsError{errorCode: ErrMissingString}
 	}
 	arg := i.next()
 	s.stringVal = arg
 	return nil
 }
 
-func (s *stringArgumentMarshaler) get() interface{} {
+func (s *stringargumentMarshaler) get() interface{} {
 	return s.stringVal
 }
 
-type integerArgumentMarshaler struct {
+type integerargumentMarshaler struct {
 	intVal int
 }
 
-func (m *integerArgumentMarshaler) set(i *iterator) error {
+func (m *integerargumentMarshaler) set(i *iterator) error {
 	if !i.hasNext() {
-		return &argsError{errorCode: ErrorCodeMissingInteger}
+		return &ArgsError{errorCode: ErrMissingInteger}
 	}
 
 	param := i.next()
 	intVal, err := strconv.Atoi(param)
 	if err != nil {
-		return &argsError{
-			errorCode:      ErrorCodeInvalidInteger,
+		return &ArgsError{
+			errorCode:      ErrInvalidInteger,
 			errorParameter: param,
 		}
 	}
@@ -310,24 +326,24 @@ func (m *integerArgumentMarshaler) set(i *iterator) error {
 	return nil
 }
 
-func (m *integerArgumentMarshaler) get() interface{} {
+func (m *integerargumentMarshaler) get() interface{} {
 	return m.intVal
 }
 
-type floatArgumentMarshaler struct {
+type floatargumentMarshaler struct {
 	floatVal float64
 }
 
-func (m *floatArgumentMarshaler) set(i *iterator) error {
+func (m *floatargumentMarshaler) set(i *iterator) error {
 	if !i.hasNext() {
-		return &argsError{errorCode: ErrorCodeMissingFloat}
+		return &ArgsError{errorCode: ErrMissingFloat}
 	}
 
 	param := i.next()
 	floatVal, err := strconv.ParseFloat(param, 64)
 	if err != nil {
-		return &argsError{
-			errorCode:      ErrorCodeInvalidFloat,
+		return &ArgsError{
+			errorCode:      ErrInvalidFloat,
 			errorParameter: param,
 		}
 	}
@@ -335,7 +351,7 @@ func (m *floatArgumentMarshaler) set(i *iterator) error {
 	return nil
 }
 
-func (m *floatArgumentMarshaler) get() interface{} {
+func (m *floatargumentMarshaler) get() interface{} {
 	return m.floatVal
 }
 
--- args_test.go
+++ args_test.go
@@ -4,86 +4,109 @@ import (
 	"testing"
 )
 
-func TestCanParseBoolArg(t *testing.T) {
-	a, err := NewArgs("l", []string{"-l"})
-	if err != nil {
-		t.Fail()
-	}
-
-	if ok := a.GetBoolean('l'); !ok {
-		t.Fail()
-	}
+func TestCreateWithNoSchemaOrArguments(t *testing.T) {
+	a, _ := NewArgs("", []string{})
+	assertEquals(t, 0, a.Cardinality(), "Exptected 0 cardinality")
 }
 
-func TestCanParseStringArg(t *testing.T) {
-	a, err := NewArgs("d*", []string{"-d", "testing"})
-	if err != nil {
-		t.Errorf("Could not parse args")
-	}
-	if "testing" != a.GetString('d') {
-		t.Errorf("Incorrect string value")
-	}
+func TestWithNoSchemaButWithOneArgument(t *testing.T) {
+	_, err := NewArgs("", []string{"-x"})
+	assertEquals(t, ErrUnexpectedArgument, err.(*ArgsError).Code(), "")
+	assertEquals(t, 'x', err.(*ArgsError).ArgumentID(), "Unexpected argument")
 }
 
-func TestCanParseInteger(t *testing.T) {
-	_, err := NewArgs("d#", []string{"-d", "1"})
-	if err != nil {
-		t.Errorf(err.Error())
-	}
+func TestWithNoSchemaButWithMultipleArguments(t *testing.T) {
+	_, err := NewArgs("", []string{"-x", "-y"})
+	assertEquals(t, ErrUnexpectedArgument, err.(*ArgsError).Code(), "Unexpected argument")
+	assertEquals(t, 'x', err.(*ArgsError).ArgumentID(), "Unexpected argument")
 }
 
-func TestCanParseBothStringAndBoolArg(t *testing.T) {
-	a, err := NewArgs("l,d*", []string{"-l", "-d", "testing"})
-	if err != nil {
-		t.Errorf("Could not parse args")
-	}
-
-	if ok := a.GetBoolean('l'); !ok {
-		t.Errorf("Invalid boolean arg")
-	}
-
-	if "testing" != a.GetString('d') {
-		t.Errorf("Invalid string arg")
-	}
+func TestNonLetterSchema(t *testing.T) {
+	_, err := NewArgs("*", []string{})
+	assertEquals(t, ErrInvalidArgumentName, err.(*ArgsError).Code(), "Invalid argument name")
+	assertEquals(t, '*', err.(*ArgsError).ArgumentID(), "Invalid argument name")
 }
 
-func TestDoublePresent(t *testing.T) {
+func TestInvalidArgumentFormat(t *testing.T) {
+	_, err := NewArgs("f-", []string{})
+	assertEquals(t, ErrInvalidFormat, err.(*ArgsError).Code(), "Invalid format")
+	assertEquals(t, 'f', err.(*ArgsError).ArgumentID(), "Invalid format")
+}
+
+func TestSimpleBooleanPresent(t *testing.T) {
+	a, err := NewArgs("x", []string{"-x"})
+	assertNil(t, err, "Expected nil")
+	assertEquals(t, 1, a.Cardinality(), "Expected 1 argument")
+	assertEquals(t, true, a.GetBoolean('x'), "Expected true")
+}
+
+func TestSimpleStringPresent(t *testing.T) {
+	a, err := NewArgs("x*", []string{"-x", "test"})
+	assertNil(t, err, "Expected nil")
+	assertEquals(t, 1, a.Cardinality(), "Expected 1 argument")
+	assertEquals(t, "test", a.GetString('x'), "Expected true")
+}
+
+func TestMissingStringParameter(t *testing.T) {
+	_, err := NewArgs("x*", []string{"-x"})
+	assertEquals(t, ErrMissingString, err.(*ArgsError).Code(), "Expected missing string")
+	assertEquals(t, 'x', err.(*ArgsError).ArgumentID(), "Expected 'x' as missing")
+}
+
+func TestSpacesInFormat(t *testing.T) {
+	a, err := NewArgs("x, y", []string{"-xy"})
+	assertNil(t, err, "Expecting no errors")
+	assertEquals(t, 2, a.Cardinality(), "Expected 2 in cardinality")
+	assertEquals(t, true, a.Has('x'), "Expected x in args")
+	assertEquals(t, true, a.Has('y'), "Expected y in args")
+}
+
+func TestSimpleIntPresent(t *testing.T) {
+	a, err := NewArgs("x#", []string{"-x", "1"})
+	assertNil(t, err, "Expected no errors")
+	assertEquals(t, 1, a.Cardinality(), "Expected 1 argument")
+	assertEquals(t, 1, a.GetInteger('x'), "Expected 1")
+}
+
+func TestInvalidIntParameter(t *testing.T) {
+	_, err := NewArgs("x#", []string{"-x", "Forty two"})
+	assertEquals(t, ErrInvalidInteger, err.(*ArgsError).Code(), "Expected invalid integer")
+	assertEquals(t, 'x', err.(*ArgsError).ArgumentID(), "Expected 'x' as invalid")
+}
+
+func TestMissingIntParameter(t *testing.T) {
+	_, err := NewArgs("x#", []string{"-x"})
+	assertEquals(t, ErrMissingInteger, err.(*ArgsError).Code(), "Expected missing integer")
+	assertEquals(t, 'x', err.(*ArgsError).ArgumentID(), "Expected 'x' as missing")
+}
+
+func TestSimpleFloatPresent(t *testing.T) {
 	a, err := NewArgs("x##", []string{"-x", "42.3"})
-	if err != nil {
-		t.Errorf(err.Error())
-	}
-
-	if !a.Has('x') {
-		t.Errorf("Element not found")
-	}
-
-	if a.Cardinality() != 1 {
-		t.Errorf("Invalid number of arguments found")
-	}
-
-	if 42.3 != a.GetDouble('x') {
-		t.Errorf("Value is invalid")
-	}
+	assertNil(t, err, "Expected no errors")
+	assertEquals(t, 1, a.Cardinality(), "Expected 1 argument")
+	assertEquals(t, 42.3, a.GetDouble('x'), "Expected 42.3")
 }
 
-func TestMissingDouble(t *testing.T) {
+func TestInvalidFloatParameter(t *testing.T) {
+	_, err := NewArgs("x##", []string{"-x", "Forty two"})
+	assertEquals(t, ErrInvalidFloat, err.(*ArgsError).Code(), "Expected invalid double")
+	assertEquals(t, 'x', err.(*ArgsError).ArgumentID(), "Expected 'x' as invalid")
+}
+
+func TestMissingFloatParameter(t *testing.T) {
 	_, err := NewArgs("x##", []string{"-x"})
-	if err != nil {
-		if err.Error() != "Could not find double parameter for -x" {
-			t.Errorf("Got: %s", err.Error())
-		}
-	} else {
-		t.Fail()
+	assertEquals(t, ErrMissingFloat, err.(*ArgsError).Code(), "Expected missing float")
+	assertEquals(t, 'x', err.(*ArgsError).ArgumentID(), "Expected 'x' as missing")
+}
+
+func assertEquals(t *testing.T, a interface{}, b interface{}, message string) {
+	if a != b {
+		t.Fatalf(message)
 	}
 }
 
-func TestBla(t *testing.T) {
-	a, err := NewArgs("d*", []string{"-d", "testing", "bla"})
-	if err != nil {
-		t.Errorf(err.Error())
-	}
-
-	if "testing" != a.GetString('d') {
-		t.Errorf("Invalid string arg")
+func assertNil(t *testing.T, a interface{}, message string) {
+	if a != nil {
+		t.Fatalf(message)
 	}
 }
